e93362c30375071a0a18d26b3ec50e3d798033cf
NikitosPes <kutsokon95@gmail.com>
2023-02-01 10:34:02 +0300

    refactor: use consts in password-complexity component

--- src/app/password-complexity/password-complexity.component.ts
+++ src/app/password-complexity/password-complexity.component.ts
@@ -6,6 +6,11 @@ enum PasswordStrength {
   STRONG = 'strong',
 }
 
+enum InvalidPasswordStatus {
+  EMPTY = 'empty',
+  SHORT = 'short'
+}
+
 @Component({
   selector: 'app-password-complexity',
   templateUrl: './password-complexity.component.html',
@@ -15,31 +20,33 @@ export class PasswordComplexityComponent {
   password!: string;
 
   readonly PASSWORD_LENGTH = 8;
-  passwordIsShort: boolean = false;
   passwordStrength: PasswordStrength | null = null;
+  invalidPasswordStatus: InvalidPasswordStatus | null = null;
 
   ngOnChanges(changes: SimpleChanges) {
-    this.setPassWordStrength(changes['password'].currentValue);
-    this.passwordIsShort = this.password.length < this.PASSWORD_LENGTH && this.password != '';
+    this.passwordStrength = this.getPassWordStrength(changes['password'].currentValue);
+    this.invalidPasswordStatus = this.getIvalidPasswordStatus(changes['password'].currentValue);
   }
 
+  // All passwords must be greater than the given length - PASSWORD_LENGTH
   // easy = letters/digits/symbols
   // strong = letters&digits&symbols
   // medium = letters&digits/letters&symbols/symbols&digits
-  easyStrengthPasswordRegexp: RegExp = /^([a-z]|[0-9]|[#?!@$%^&*-]).{8,}$/;
-  strongStrengthPasswordRegexp: RegExp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-  mediumStrengthPasswordRegexp: RegExp =/^((?=.*?[a-z])(?=.*?[0-9])|(?=.*?[a-z])(?=.*?[#?!@$%^&*-])|(?=.*?[#?!@$%^&*-])(?=.*?[0-9])).{8,}$/;
+  easyStrengthPasswordRegexp: RegExp = new RegExp(`^([a-z]|[0-9]|[#?!@$%^&*-]).{${this.PASSWORD_LENGTH},}$`);
+  strongStrengthPasswordRegexp: RegExp = new RegExp(`^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[@$!%*#?&]).{${this.PASSWORD_LENGTH},}$`);
+  mediumStrengthPasswordRegexp: RegExp = new RegExp(`^((?=.*?[a-z])(?=.*?[0-9])|(?=.*?[a-z])(?=.*?[#?!@$%^&*-])|(?=.*?[#?!@$%^&*-])(?=.*?[0-9])).{${this.PASSWORD_LENGTH},}$`);
 
-  setPassWordStrength(password: string) {
-    if (!password) {
-      this.passwordStrength = null;
-      return
-    } 
-    if (this.easyStrengthPasswordRegexp.test(password))
-      this.passwordStrength = PasswordStrength.EASY;
-    if (this.mediumStrengthPasswordRegexp.test(password))
-      this.passwordStrength = PasswordStrength.MEDIUM;
-    if (this.strongStrengthPasswordRegexp.test(password))
-      this.passwordStrength = PasswordStrength.STRONG;
+  getPassWordStrength(password: string): PasswordStrength | null {
+    console.log(this.strongStrengthPasswordRegexp.source)
+    if (this.strongStrengthPasswordRegexp.test(password)) return PasswordStrength.STRONG;
+    if (this.mediumStrengthPasswordRegexp.test(password)) return PasswordStrength.MEDIUM;
+    if (this.easyStrengthPasswordRegexp.test(password)) return PasswordStrength.EASY;
+    return null;  
+  }
+
+  getIvalidPasswordStatus(password: string): InvalidPasswordStatus | null {
+    if (!password) return InvalidPasswordStatus.EMPTY;
+    if (password.length < this.PASSWORD_LENGTH) return InvalidPasswordStatus.SHORT;
+    return null;
   }
 }
